1469d6714fda658a984f668521cd9a1b897695e1
Douglas-Chalmers <douglas.chalmers1@gmail.com>
2023-02-01 19:56:20 -0600

    first commit

--- src/components.rs
+++ src/components.rs
@@ -2,12 +2,37 @@ pub use crate::prelude::*;
 use crate::{ColorPair, FontCharType};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-
-pub struct Render{
+pub struct Render {
     pub color: ColorPair,
-    pub glyph: FontCharType
+    pub glyph: FontCharType,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Player;
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Enemy;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct MovingRandomly;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct WantsToMove {
+    pub entity: Entity,
+    pub destination: Point
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Health{
+    pub current: i32,
+    pub max: i32
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Name(pub String);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToAttack {
+    pub attacker: Entity,
+    pub victim: Entity
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,40 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-mod map;
-mod map_builder;
 mod camera;
 mod components;
+mod map;
+mod map_builder;
 mod spawner;
 mod systems;
-
-
+mod turn_state;
 
 mod prelude {
-    pub use bracket_lib::prelude::*;
-    pub use legion::*;
-    pub use legion::world::SubWorld;
-    pub use legion::systems::CommandBuffer;
+    pub use crate::camera::*;
     pub use crate::components::*;
-    pub use crate::spawner::*;
-    pub use crate::systems::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::camera::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
+    pub use crate::turn_state::*;
+    pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
 }
 
-use std::thread::spawn;
 use prelude::*;
+use std::thread::spawn;
 
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule,
 }
 
 impl State {
@@ -42,24 +44,47 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
         Self {
             ecs,
             resources,
-            systems: build_scheduler()
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler(),
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        self.resources.insert(ctx.key);
         ctx.set_active_console(0);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        ctx.set_active_console(2);
+        ctx.cls();
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_state{
+            TurnState::AwaitingInput => self.input_systems.execute(
+            &mut self.ecs,
+            &mut self.resources
+            ),
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs, &mut self.resources);
+            }
+            TurnState::MonsterTurn => {
+                self.monster_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+        }
         render_draw_buffer(ctx).expect("Render error");
     }
 }
@@ -68,11 +93,13 @@ fn main() -> BError {
         .with_title("Rusty Rogue")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-        .with_tile_dimensions(32,32)
+        .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
-        .with_font("dungeonfont.png", 32,32)
+        .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8,8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH*2, SCREEN_HEIGHT*2, "terminal8x8.png")
         .build()?;
     main_loop(context, State::new())
 }
